02e0199daa3435e110eacc375735034805aa9509
Anton Antonov <tonik32@gmail.com>
2022-07-28 23:30:48 +0300

    Описывает основную модель приложения

--- backend/foodgram/recipe/models.py
+++ backend/foodgram/recipe/models.py
@@ -1,3 +1,146 @@
+from enum import unique
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.core import validators
 
-# Create your models here.
+
+User = get_user_model()
+
+
+class Units(models.Model):
+    title = models.CharField(
+        'Единицы измерения',
+        max_length=128
+    )
+
+    class Meta:
+        unique = ['name']
+
+
+class Tag(models.Model):
+    title = models.CharField(
+        'Название Тега',
+        max_length=200,
+    )
+    slug = models.SlugField(
+        max_length=100,
+        unique=True,
+    )
+    color = models.CharField(
+        'Цвет',
+        max_length=7,
+        unique=True)
+
+
+class Ingridients(models.Model):
+    pass
+    title = models.CharField(
+        'Название ингридиента',
+        max_length=256
+    )
+    total = models.C
+    units = models.ForeignKey(
+        Units,
+        on_delete=models.SET_NULL,
+        blank=False,
+        null=False,
+        related_name='ingridients',
+    )
+    amount = models.PositiveSmallIntegerField(
+        default=1,
+        validators=(
+            validators.MinValueValidator(
+                1, message='Мин. количество ингридиентов 1'),),
+        verbose_name='Количество',)
+
+
+class Follow(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='follower',
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='following',
+    )
+
+    class Meta:
+        unique_together = ['user', 'author']
+
+    def __str__(self):
+        return self.author.username
+
+class Recipe(models.Model):
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='recipe',
+        verbose_name='Автор',
+    )
+    title = models.CharField(
+        'Название рецепта',
+        max_length=255,
+    )
+    image = models.ImageField(
+        'Изображение рецепта',
+        upload_to='static/recipe/',
+        blank=True,
+        null=True,
+    )
+    text = models.TextField(
+        'Описание рецепта',
+    )
+    cooking_time = models.BigIntegerField(
+        'Время приготовления рецепта',
+    )
+    ingredients = models.ManyToManyField(
+        Ingridients,
+        through='RecipeIngredient',
+    )
+    tags = models.ManyToManyField(
+        Tag,
+        related_name='recipes',
+    )
+    cooking_time = models.PositiveSmallIntegerField(
+        verbose_name='Время приготовления в минутах',
+        validators=[
+            validators.MinValueValidator(
+                1,
+                message='Мин. время приготовления 1 минута'
+            ), 
+        ]
+    )
+    pub_date = models.DateTimeField(
+        'Дата публикации',
+        auto_now_add=True,
+    )
+    
+class FavoriteRecipe(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        null=True,
+        related_name='favorite_recipe',
+    )
+    recipe = models.ManyToManyField(
+        Recipe,
+        related_name='favorite_recipe',
+    )
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='cart',
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='cart',
+
+    )
+    class Meta:
+        unique_together = ['user', 'recipe']
