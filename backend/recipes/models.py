bd8f10c409c26334dbdba0ad0098f27d883073f9
Anton Antonov <tonik32@gmail.com>
2022-08-07 13:28:06 +0300

    commit

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -171,13 +171,10 @@ class FavoriteRecipe(models.Model):
         return f'Пользователь {self.user} добавил {list_} в избранные.'
 
     @receiver(post_save, sender=User)
-    def create_favorite_recipe(
-        self,
-        sender,
-        instance,
-        created,
-        **kwargs
-    ):
+    def create_favorite_recipe(sender,
+                               instance,
+                               created,
+                               **kwargs):
         if created:
             return FavoriteRecipe.objects.create(user=instance)
 
